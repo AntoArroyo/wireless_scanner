13e1abe138c0b99e3eb08652808dcc631028a21d
AntoArroyo <arroyo.antom@gmail.com>
2025-07-03 19:57:33 +0200

    Add launch file for wifi scan with iw

---
+++ launch/wireless_scanner.launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='wireless_scanner',
+            executable='wireless_scanner',
+            name='wireless_scanner',
+            output='screen',
+            #prefix='sudo -E'
+            # Launch the node with root access (GPIO) in a shell
+            prefix=["sudo -E env \"PYTHONPATH=$PYTHONPATH\" \"LD_LIBRARY_PATH=$LD_LIBRARY_PATH\" \"PATH=$PATH\" \"USER=$USER\"  bash -c "],
+            shell=True,
+        )
+    ])
